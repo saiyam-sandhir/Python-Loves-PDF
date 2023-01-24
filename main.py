9065343e4ec4084175a48f94117caebc11e141c8
Saiyam-Sandhir <saiyamsandhir@gmail.com>
2023-01-24 22:26:50 +0530

    Add functionality to buttons in the main menu window
    resolve #8

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import *
+from PDF2IMG import Pdf2Image_Win
+from IMG2PDF import Image2Pdf_Win
 
 BG_COL = {
     "header": "#7286D3",
@@ -9,6 +11,7 @@ TXT_COL = {
     "header": "#FFFBF5",
     "body": "#03001C"
 }
+col_theme = [BG_COL, TXT_COL]
 
 main = Tk()
 
@@ -31,13 +34,13 @@ Grid.columnconfigure(main_body_LabelFrame_pdfNimg, 0, weight = 1)
 Grid.columnconfigure(main_body_LabelFrame_pdfNimg, 1, weight = 1)
 
 pdf2img_image = PhotoImage(file = "images\\pdf2img.png")
-main_body_Pdf2Img_button = Button(main_body_LabelFrame_pdfNimg, image = pdf2img_image)
+main_body_Pdf2Img_button = Button(main_body_LabelFrame_pdfNimg, image = pdf2img_image, command = lambda: Pdf2Image_Win(main, col_theme))
 main_body_Pdf2Img_button.grid(column = 0, row = 0, pady = (10, 5))
 main_body_Pdf2Img_Label = Label(main_body_LabelFrame_pdfNimg, text = "PDF ➡ Image", font = ("Arial", 15), background = BG_COL["body"], foreground = TXT_COL["body"])
 main_body_Pdf2Img_Label.grid(column = 0, row = 1, pady = (0, 10))
 
 img2pdf_image = PhotoImage(file = "images\\img2pdf.png")
-main_body_Img2Pdf_button = Button(main_body_LabelFrame_pdfNimg, image = img2pdf_image)
+main_body_Img2Pdf_button = Button(main_body_LabelFrame_pdfNimg, image = img2pdf_image, command = lambda: Image2Pdf_Win(main, col_theme))
 main_body_Img2Pdf_button.grid(column = 1, row = 0, pady = (10, 5))
 main_body_Img2Pdf_Label = Label(main_body_LabelFrame_pdfNimg, text = "Image ➡ PDF", font = ("Arial", 15), background = BG_COL["body"], foreground = TXT_COL["body"])
 main_body_Img2Pdf_Label.grid(column = 1, row = 1, pady = (0, 10))
