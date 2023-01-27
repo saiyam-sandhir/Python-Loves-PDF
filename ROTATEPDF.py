521caa03ddd55238279a243711097b812715a0ba
Saiyam-Sandhir <saiyamsandhir@gmail.com>
2023-01-27 23:22:59 +0530

    Design rotate pdf window
    resolve #20

--- ROTATEPDF.py
+++ ROTATEPDF.py
@@ -1,4 +1,5 @@
 import PyPDF2
+from tkinter import *
   
 def PDFrotate(origFileName, newFile_path, rotation):
   
@@ -18,3 +19,30 @@ def PDFrotate(origFileName, newFile_path, rotation):
   
     with open(newFile_path + "\\pdf_rotate.pdf", "wb") as f:
     	pdfWriter.write(newFile_path + "\\pdf_rotate.pdf")
+
+class RotatePdf_Win(Toplevel):
+    def __init__(self, master, theme: list):
+        super().__init__(master)
+        BG_COL = theme[0][0]
+        TXT_COL = theme[0][1]
+
+        HEADER_TXT = theme[1][0]
+        BODY_TXT = theme[1][1]
+
+        self.title("Rotate PDF pages")
+        self.iconbitmap("images\\icon.ico")
+
+        header = Frame(self, bg = BG_COL["header"])
+        header.pack(fill = X)
+        header_label = Label(header, text = "🔄 📄", font = HEADER_TXT, background = BG_COL["header"], foreground = TXT_COL["header"])
+        header_label.pack(pady = (0, 15))
+
+        body = Frame(self, bg = BG_COL["body"], height = 400)
+        body.pack(fill = BOTH, expand = 1)
+
+        self.body_select_button = Button(body, text = "Select PDF file", font = BODY_TXT)
+        self.body_select_button.pack(fill = BOTH, padx = 10, pady = 10, expand = 1)
+
+        body_save_button = Button(body, text = "Save PDF...", font = BODY_TXT)
+        body_save_button.pack(fil = BOTH, padx = 10, pady = 10, expand = 1)
+        
